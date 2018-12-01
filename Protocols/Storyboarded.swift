5b39983f8df17789cb7c7b4a9054561dcb6d7cba
Jeff Cedilla <spaghetticoderapps@gmail.com>
2018-11-30 20:46:33 -0800

    remove default entry point for app, override appdelegate

--- Protocols/Storyboarded.swift
+++ Protocols/Storyboarded.swift
@@ -11,7 +11,7 @@ public protocol Storyboarded {
     static func instantiate(storyboardName: String) -> Self
 }
 
-extension Storyboarded where Self: UIViewController {
+public extension Storyboarded where Self: UIViewController {
     static func instantiate(storyboardName: String) -> Self {
         // this pulls out "MyApp.MyViewController"
         let fullName = NSStringFromClass(self)
