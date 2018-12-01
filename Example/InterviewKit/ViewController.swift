c7753f7762bbb316db4fc42170a7a5d7eef53c11
Jeff Cedilla <spaghetticoderapps@gmail.com>
2018-11-30 19:15:11 -0800

    Initial commit

---
+++ Example/InterviewKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  InterviewKit
+//
+//  Created by spaghetticoderapps@gmail.com on 11/30/2018.
+//  Copyright (c) 2018 spaghetticoderapps@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
