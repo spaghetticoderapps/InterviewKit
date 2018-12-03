7bab1b5bcb3bb92dc64320ffb8d734d45452c7d5
Jeff Cedilla <spaghetticoderapps@gmail.com>
2018-12-02 21:09:21 -0800

    add image caching

--- InterviewKit/Classes/URLSession/UIImageView+Download.swift
+++ InterviewKit/Classes/URLSession/UIImageView+Download.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+class IKImageCache {
+    static let shared = IKImageCache()
+    
+    let cache = NSCache<AnyObject, AnyObject>()
+}
+
 public extension UIImageView {
     func download(from url: String) {
         alpha = 0
+        image = nil
+        
         guard let url = URL(string: url) else {
             UIView.animate(withDuration: 0.7) {
                 self.alpha = 1
@@ -17,11 +25,22 @@ public extension UIImageView {
             }
             return
         }
+        
+        if let cachedImage = IKImageCache.shared.cache.object(forKey: url as AnyObject) as? UIImage {
+            UIView.animate(withDuration: 0.7) {
+                self.alpha = 1
+                self.image = cachedImage
+            }
+            return
+        }
+        
         DispatchQueue.global().async { [weak self] in
             guard let strongSelf = self else { return }
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
+                        IKImageCache.shared.cache.setObject(image, forKey: url as AnyObject)
+                        
                         UIView.animate(withDuration: 0.7) {
                             strongSelf.alpha = 1
                             strongSelf.image = image
