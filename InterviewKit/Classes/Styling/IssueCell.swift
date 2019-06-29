0f495ec484a679c0646b7b2d9a9f1848ec7594f4
Jeff Cedilla <spaghetticoderapps@gmail.com>
2019-06-29 13:21:15 -0700

    add styling

---
+++ InterviewKit/Classes/Styling/IssueCell.swift
@@ -0,0 +1,24 @@
+//
+//  IssueCell.swift
+//  RepoLister
+//
+//  Created by Jeff Cedilla on 6/29/19.
+//  Copyright © 2019 Jeff Cedilla. All rights reserved.
+//
+
+import UIKit
+
+class IssueCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InterviewKit/Classes/Styling/UIColor+Styling.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Styling.swift
+//  InterviewKit
+//
+//  Created by Jeff Cedilla on 6/29/19.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    static let lightBlue = UIColor(red:0.13, green:0.42, blue:0.73, alpha:1.00)
+    
+}
